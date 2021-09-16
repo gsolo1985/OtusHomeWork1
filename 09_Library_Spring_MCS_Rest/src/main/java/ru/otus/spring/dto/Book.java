dcefd2f77d5e18bac3b7e2573bdcc030eca83dcf
Георгий Соло <gsolo@diasoft.ru>
2021-09-16 15:36:06 +0300

    create pull request №17

--- 09_Library_Spring_MCS_Rest/src/main/java/ru/otus/spring/dto/Book.java
+++ 09_Library_Spring_MCS_Rest/src/main/java/ru/otus/spring/dto/Book.java
@@ -1,12 +1,16 @@
 package ru.otus.spring.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
     private long id;
     private String title;
